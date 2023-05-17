d9ebfffb53d8667175f0039dceea27521fcb284e
Sotiris <it22131@hua.gr>
2023-05-18 00:59:15 +0300

    Problem with albums 18/5 early

--- base.py
+++ base.py
@@ -12,23 +12,23 @@ port = '3307'
 api_key = '899c27976ecfa7b4adf9f276b445d62f'
 
 # Create an instance of the DatabaseConnector
-db_connector = DatabaseConnector(host, username, password, database ,port)
+db_connector = DatabaseConnector(host, username, password, database, port)
 db_connector.connect()
 
 # Create an instance of the LastFmAPI
 lastfm_api = LastFmAPI(api_key)
 
 # Example query execution
-#query = "INSERT INTO Users (username, country) VALUES ('John', 'john@example.com')"
-#db_connector.execute_query(query)
-#db_connector.commit_changes()
+# query = "INSERT INTO Users (username, country) VALUES ('John', 'john@example.com')"
+# db_connector.execute_query(query)
+# db_connector.commit_changes()
 
-usernames = ['Robert', 'Chris', 'Lopes','Lia','Nicole','RJ']
+usernames = ['Robert', 'Chris', 'Lopes', 'Lia', 'Nicole', 'RJ']
 count = len(usernames)
 
-while count >0:
+while count > 0:
     user_info = lastfm_api.get_user_info(usernames[count-1])
-    
+
     if user_info is not None:
         name = user_info['user']['name']
         country = user_info['user']['country']
@@ -38,23 +38,39 @@ while count >0:
     count -= 1
 
 count = len(usernames)
-while count >0:
+artists_sum = 0
+artists_names = []
+while count > 0:
     user_top_artists = lastfm_api.get_user_top_artists(usernames[count-1])
 
     top_artists = user_top_artists['topartists']['artist']
 
     for artist in top_artists:
+        artists_sum += 1
         artist_id = artist['mbid']
         artist_name = artist['name']
-        print('A , B : ', artist_id , artist_name)
+        artists_names.append(artist_name)
+        print('A , B : ', artist_id, artist_name , artists_names[0])
         query = f"INSERT INTO MusicGroups (group_id, group_name) VALUES ('{artist_id}', '{artist_name}')"
         db_connector.execute_query(query)
         db_connector.commit_changes()
     count -= 1
 
+count = 6
+while count > 0:
+    artists_albums = lastfm_api.get_artists_top_albums(artists_names.pop(1))
 
+    top_albums = artists_albums['topalbums']['album']
 
-
+     # Iterate over the top albums
+    for album in top_albums:
+        album_name = album['name']
+        artist_name = album['artist']['name']
+        print('A , B : ', album_name, artist_name)
+        query = f"INSERT INTO Albums (album_name, artist_name) VALUES ('{album_name}', '{artist_name}')"
+        db_connector.execute_query(query)
+        db_connector.commit_changes()
+    count -= 1
 
 
 # Disconnect from the databas
--- lastfm_api.py
+++ lastfm_api.py
@@ -10,10 +10,20 @@ class LastFmAPI:
         if response.status_code == 200:
             return response.json()
         else:
+            print("Something went wrong on the request")
             return None
     
     def get_user_top_artists(self, username):
         url = f"http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user={username}&api_key={self.api_key}&format=json"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Something went wrong on the request")
+            return None
+    
+    def get_artists_top_albums(self, artists_name):
+        url = f"http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist={artists_name}&api_key={self.api_key}&format=json"
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
