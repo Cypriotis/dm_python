06710f0d9e9513a0f8319a172d807c64ae33bd65
Sotiris <it22131@hua.gr>
2023-05-29 19:51:19 +0300

    Filling all tables on database

--- base.py
+++ base.py
@@ -27,7 +27,7 @@ db_connector.execute_query('''DROP TABLE UsersFavInfo''')
 #creating tables if not exists
 db_connector.execute_query('''CREATE TABLE IF NOT EXISTS Users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(254), country VARCHAR(254), favorite_artist VARCHAR(254))''')
 
-db_connector.execute_query('''CREATE TABLE IF NOT EXISTS Artists (artists_name VARCHAR(254),mbid VARCHAR(254),artists_url VARCHAR(254))''')
+db_connector.execute_query('''CREATE TABLE IF NOT EXISTS Artists (artists_name VARCHAR(254),mbid VARCHAR(254),artist_url VARCHAR(254))''')
 
 db_connector.execute_query('''CREATE TABLE IF NOT EXISTS Albums(artists_name VARCHAR(254),album_name VARCHAR(254))''')
 
@@ -52,6 +52,14 @@ while count > 0:
         print(current_name)
         country = user_info['user']['country']
         artist_name = top_artists[0]['name']
+        mbid = top_artists[0]['mbid']
+        artist_url = top_artists[0]['url']
+
+        print("mbid : " , mbid , "url: " , artist_url)
+
+        query = f"INSERT INTO Artists (artists_name, mbid,artist_url) VALUES ('{artist_name}','{mbid}','{artist_url}')"
+        db_connector.execute_query(query)
+        db_connector.commit_changes()
         query = f"INSERT INTO Users (username, country , favorite_artist) VALUES ('{name}', '{country}' , '{artist_name}')"
         db_connector.execute_query(query)
         db_connector.commit_changes()
@@ -63,47 +71,13 @@ while count > 0:
             if(flag>0):
              album_name = artists_albums['topalbums']['album'][flag]['name']
              query = f"INSERT INTO UsersFavInfo (user_name , artists_name, album_name) VALUES ('{current_name}', '{artist_name}', '{album_name}')"
+             query2 = f"INSERT INTO Albums (artists_name ,album_name ) VALUES ('{artist_name}','{album_name}')"
              db_connector.execute_query(query)
+             db_connector.execute_query(query2)
              db_connector.commit_changes()
              print(album_name)
 
         count -= 1
-
-count = len(usernames)
-artists_sum = 0
-artists_names = []
-while count > 0:
-    user_top_artists = lastfm_api.get_user_top_artists(usernames[count-1])
-
-    top_artists = user_top_artists['topartists']['artist']
-
-    for artist in top_artists:
-        artists_sum += 1
-        artist_id = artist['mbid']
-        artist_name = artist['name']
-        artists_names.append(artist_name)
-        print('A , B : ', artist_id, artist_name )
-        query = f"INSERT INTO MusicGroups (group_id, group_name) VALUES ('{artist_id}', '{artist_name}')"
-        db_connector.execute_query(query)
-        db_connector.commit_changes()
-    count -= 1
-
-##count = 6
-while count > 0:
-    artists_albums = lastfm_api.get_artists_top_albums(artists_names.pop(1))
-
-    top_albums = artists_albums['topalbums']['album']
-    print("HERE : " ,top_albums)
-
-     # Iterate over the top albums
-    #for album in top_albums:
-    #    album_name = album['name']
-    #    artist_name = album['artist']['name']
-    #    print('A , B : ', album_name, artist_name)
-    #    query = f"INSERT INTO Albums (album_name, artist_name) VALUES ('{album_name}', '{artist_name}')"
-    #    db_connector.execute_query(query)
-    #    db_connector.commit_changes()
-    #count -= 1
 
 
 # Disconnect from the databas
