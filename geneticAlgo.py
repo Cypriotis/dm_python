b5a5039735fba2d92cf59be8050941ec5020a4d5
Sotiris <it22131@hua.gr>
2023-06-10 12:53:31 +0300

    Small changes on last question..added sum of money spend for each user on final colum of each row on user_selections

--- geneticAlgo.py
+++ geneticAlgo.py
@@ -2,11 +2,11 @@ import csv
 import random
 
 # Load user balance and desired rates
-with open('user_money_rates.csv', 'r') as user_file:
+with open('./csv/user_money_rates.csv', 'r') as user_file:
     user_data = list(csv.reader(user_file))
 
 # Load album prices
-with open('album_price.csv', 'r') as album_file:
+with open('./csv/album_price.csv', 'r') as album_file:
     album_data = list(csv.reader(album_file))
 
 # Convert album prices to integers
@@ -22,8 +22,8 @@ album_ids = list(range(1, num_albums + 1))
 random.shuffle(album_ids)
 selected_albums = album_ids[:50]
 
-# Initialize a list to store the selected albums for each user
-user_albums = []
+# Initialize a list to store the selected albums and total spending for each user
+user_selections = []
 
 # Iterate over each user
 for user in user_data:
@@ -34,19 +34,24 @@ for user in user_data:
     albums = list(zip(selected_albums, album_prices))
 
     # Sort albums based on the desired rates (in descending order)
-    albums.sort(key=lambda x: desired_rates[x[0] - 1], reverse=True)
+    albums.sort(key=lambda x: desired_rates[x[1] - 1], reverse=True)
 
     # Select albums that can be afforded within the available balance
     selected = []
+    total_spending = 0
     for album in albums:
         if available_balance >= album[1]:
             selected.append(album[0])
+            total_spending += album[1]
             available_balance -= album[1]
 
-    # Store the selected albums for the user
-    user_albums.append(selected)
+    # Store the selected albums and total spending for the user
+    user_selections.append((selected, total_spending))
 
-# Export the selected albums for each user
-for i, albums in enumerate(user_albums):
-    user_id = i + 1
-    print(f"User {user_id}: Selected Albums - {albums}")
+# Export the selected albums and total spending for each user to a file
+output_file = './csv/user_selections.csv'
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for i, (albums, total_spending) in enumerate(user_selections):
+        user_id = i + 1
+        writer.writerow([f"User {user_id}"] + albums + [total_spending])
